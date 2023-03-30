c8b63ffc3e94ebed0badfd0f6609f5ed816dd930
zeeaanahmad <zeeaan.ahmad@gmail.com>
2023-03-30 17:31:43 +0500

    replace BrowserRouter to HashRoute

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import { Landing, Error, Register, ProtectedRoute } from './pages';
 import { ToastContainer } from 'react-toastify';
@@ -12,7 +12,7 @@ import {
 } from './pages/dashboard';
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route
           path='/'
@@ -32,7 +32,7 @@ function App() {
         <Route path='*' element={<Error />} />
       </Routes>
       <ToastContainer position='top-center' />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
